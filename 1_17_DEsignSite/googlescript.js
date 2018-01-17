41d20963adefff2d1085927ba4dad1afb3ddde5f
younginsa <wow0726@gmail.com>
2018-01-17 11:09:55 -0500

    project2 with name change

---
+++ 1_17_DEsignSite/googlescript.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+ $('.ideaBulb').click(function(){
+   console.log('clicked!');
+   var sketchItem = $('input[name=sketchListItem]').val();
+    $('.list').append('<div class="item">'+sketchItem+'</div>');
+    });
+
+
+    $(document).on('click','.item',function(){
+      var searchIt = $(this).text();
+      console.log('searchIt')
+      myFunction(searchIt);
+      $(this).remove();
+
+    });
+  });
+
+
+  function myFunction(searchIt) {
+
+      var myWindow = window.open("https://www.google.com/search?q="+searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+
+  }
