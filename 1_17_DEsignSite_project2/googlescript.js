9469257e0cf6b346dfcfb1e83149e239e0ed5969
younginsa <wow0726@gmail.com>
2018-01-24 09:21:03 -0500

    compilation layout with project2 ck,dbck differenciation

--- 1_17_DEsignSite_project2/googlescript.js
+++ 1_17_DEsignSite_project2/googlescript.js
@@ -4,21 +4,65 @@ $(document).ready(function(){
    console.log('clicked!');
    var sketchItem = $('input[name=sketchListItem]').val();
     $('.list').append('<div class="item">'+sketchItem+'</div>');
-    });
+  });
 
 
+/*
     $(document).on('click','.item',function(){
       var searchIt = $(this).text();
       console.log('searchIt')
       myFunction(searchIt);
-      $(this).remove();
-
+      $(this).addClass('donelist');
     });
+
+    $(document).on('dblclick','.item',function(){
+        $(this).remove();
+    });
+
+    function doClickAction() {
+      $("#click h3").append("•");
+    }
+    function doDoubleClickAction() {
+      $("#double-click h3").append("•");
+    }
+
+*/
+
+//-----------
+
+var timer = 0;
+var delay = 200;
+var prevent = false;
+
+$(document)
+  .on('click','.item',function() {
+    var searchIt = $(this).text();
+    console.log(searchIt)
+    $(this).addClass('donelist');
+    timer = setTimeout(function() {
+      if (!prevent) {
+        myFunction(searchIt);
+        doClickAction();
+      }
+      prevent = false;
+    }, delay);
+  })
+  .on('dblclick','.item', function() {
+    $(this).remove();
+    clearTimeout(timer);
+    prevent = true;
+    doDoubleClickAction();
+  });
+
+
+
+//------------
+
   });
 
 
   function myFunction(searchIt) {
 
-      var myWindow = window.open("https://www.google.com/search?q="+searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=400");
+      var myWindow = window.open("https://www.google.com/search?q="+ searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=400");
 
   }
---
+++ 1_23_Compilation/compilation.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+  var verticalGrid = '<div class="gridWidth"></div>';
+  var verticalGridYear = '<div class="gridWidthYear">'
+  var horizentalGrid = '<div class="gridHeight"></div>';
+  var screenWidth = $(window).width();
+  var screenHeight = 1500;
+  console.log("this is screenWidth " + screenWidth + "\nthis is screenheight " + screenHeight);
+
+//-----background Grid-------//
+  var iTotal=0;
+  var jTotal=0;
+  var yearTotal=1;
+  var monthTotal=0;
+
+  for(var i=0; i<screenWidth+58; i+=50){
+
+      if(iTotal%6 === 1){
+        $('.BGgridWidth').append(verticalGridYear + '<span class="yearPos">' + yearTotal +'</span>'+ "</div>");
+        yearTotal++;
+      }else{
+        $('.BGgridWidth').append(verticalGrid);
+        monthTotal++;
+      }
+      iTotal++;
+    }
+
+  console.log("year total " + yearTotal);
+  console.log("month total " + monthTotal);
+  console.log("i " + i);
+  console.log("iTotal "+ iTotal);
+
+  for(var j=0; j<screenHeight-50; j+=50){
+    $('.BGgridHeight').append(horizentalGrid);
+      jTotal++;
+    }
+  console.log("j " + j);
+  console.log("jtotal " + jTotal);
+
+
+  var topOffset = parseInt($(".yearPos").css('top'));
+  $(window).scroll(function(){
+      $('.yearPos').css({
+          'top': $(this).scrollTop() + topOffset
+      });
+  });
+
+//-----scroll Animation-------
+//   //get scroll position
+//   var scrollPosition = $(window).scrollTop();
+//   console.log('mouse '+scrollPosition);
+// //from https://www.sitepoint.com/scroll-based-animations-jquery-css3/
+//   var $animation_elements = $('.nameBlock');
+//   var $window = $(window);
+//
+//   function check_if_in_view() {
+//     var window_height = $window.height();
+//     var window_top_position = $window.scrollTop();
+//     var window_bottom_position = (window_top_position + window_height);
+//
+//     $.each($animation_elements, function() {
+//       var $element = $(this);
+//       var element_height = $element.outerHeight();
+//       var element_top_position = $element.offset().top;
+//       var element_bottom_position = (element_top_position + element_height);
+//
+//       //check to see if this current container is within viewport
+//       if ((element_bottom_position >= window_top_position) &&
+//           (element_top_position <= window_bottom_position)) {
+//         $element.addClass('in-view');
+//       } else {
+//         $element.removeClass('in-view');
+//       }
+//     });
+//   }
+//   $window.on('scroll resize', check_if_in_view);
+//   $window.trigger('scroll');
+//
+//
+//
+//
+ });
