97aecd368c91dc86bef7252583147cdd2e97f425
younginsa <wow0726@gmail.com>
2018-01-25 18:39:56 -0500

    pro2 update: draggable/ori_google link

--- 1_17_DEsignSite_project2/googlescript.js
+++ 1_17_DEsignSite_project2/googlescript.js
@@ -3,32 +3,13 @@ $(document).ready(function(){
  $('.ideaBulb').click(function(){
    console.log('clicked!');
    var sketchItem = $('input[name=sketchListItem]').val();
-    $('.list').append('<div class="item">'+sketchItem+'</div>');
+    $('.list').append('<div class="item ui-widget-content">'+sketchItem+'</div>');
+    $( ".item" ).draggable(); //ui-jQuery
+
   });
 
 
-/*
-    $(document).on('click','.item',function(){
-      var searchIt = $(this).text();
-      console.log('searchIt')
-      myFunction(searchIt);
-      $(this).addClass('donelist');
-    });
-
-    $(document).on('dblclick','.item',function(){
-        $(this).remove();
-    });
-
-    function doClickAction() {
-      $("#click h3").append("•");
-    }
-    function doDoubleClickAction() {
-      $("#double-click h3").append("•");
-    }
-
-*/
-
-//-----------
+//-----divide Click_Dbclick action--------//
 
 var timer = 0;
 var delay = 200;
@@ -42,7 +23,7 @@ $(document)
     timer = setTimeout(function() {
       if (!prevent) {
         myFunction(searchIt);
-        doClickAction();
+      //  doClickAction();
       }
       prevent = false;
     }, delay);
@@ -51,18 +32,13 @@ $(document)
     $(this).remove();
     clearTimeout(timer);
     prevent = true;
-    doDoubleClickAction();
+  //  doDoubleClickAction();
   });
-
-
-
-//------------
 
   });
 
+  //--------search clicked text--------//
 
   function myFunction(searchIt) {
-
       var myWindow = window.open("https://www.google.com/search?q="+ searchIt, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=400,height=400");
-
   }
