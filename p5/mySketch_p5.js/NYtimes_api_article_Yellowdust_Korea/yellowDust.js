67708457d7e51df79c21a583870f9e34ff70507b
younginsa <wow0726@gmail.com>
2019-03-25 21:35:31 -0400

    API fixed

--- p5/mySketch_p5.js/NYtimes_api_article_Yellowdust_Korea/yellowDust.js
+++ p5/mySketch_p5.js/NYtimes_api_article_Yellowdust_Korea/yellowDust.js
@@ -1,9 +1,11 @@
-var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=0a496657f3f0437fae3724a0cb1d7bd3&q=';
+var searchtext;
+var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchtext + '&api-key=K7dGNyHoFOuGUOepv8qW8PtvNXiOAAjt';
 var searchIt;
 
 
 function setup() {
-	url = url+'yellow dust korea';
+	//url = url+'yellow dust korea';
+	searchtext = 'election';
 	noCanvas();
 	// h1 = createElement('h1', 'sequence is important');
 	loadJSON(url, gotData);
@@ -14,12 +16,13 @@ function setup() {
 function keyPressed(){
   if (keyCode === ENTER){
   	 removeElements();
-  	 url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=0a496657f3f0437fae3724a0cb1d7bd3&q=';
 	 searchIt = $('input[name=sketchListItem]').val();
-	 url = url+searchIt;
+	 url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchIt + '&api-key=K7dGNyHoFOuGUOepv8qW8PtvNXiOAAjt';
+
+//	 url = url+searchIt;
 	 console.log(url);
 
-	 loadJSON(url, gotData);	 
+	 loadJSON(url, gotData);
 	 // e.preventDefault();
      $('input[name=sketchListItem]').val('');
 }
