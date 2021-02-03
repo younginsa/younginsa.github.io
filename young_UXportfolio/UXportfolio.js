2ebc6a08b60cbd2a1378bae21587a6557c16e809
youngin sa <younginsa@youngins-MacBook-Pro.local>
2021-02-03 16:52:51 +0900

    new location

---
+++ young_UXportfolio/UXportfolio.js
@@ -0,0 +1,47 @@
+var header = document.getElementById("myHeader");
+var content = document.getElementById("myContent");
+var myDropdown = document.getElementById("myDropdown");
+var sticky = header.offsetTop;
+var i=2;
+
+
+$(window).scroll(function(){
+  myFunction();
+});
+
+
+// Close the dropdown if the user clicks outside of it
+$(window).click(function(event){
+  var elementClasses = document.getElementById("myDropdown");
+  if(document.getElementsByClassName('dropbtn')[0].contains(event.target)){
+    i++;
+    console.log("inside clicked!", i);
+  }else if((i%2)&&(!document.getElementsByClassName('sub-menu')[0].contains(event.target))){
+    console.log("yes")
+    document.getElementById("myDropdown").classList.toggle("show");
+    i=2;
+  }
+});
+
+
+// function open and close menu by appending class show
+function menuFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// function for sticky menu onscroll
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    content.classList.add("spacing");
+  } else {
+    header.classList.remove("sticky");
+    content.classList.remove("spacing");
+  }
+}
+
+
+//code for replaing text into clicked hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4$('.sub-menu>p').click(function() {
+//    $('#menutitle').text($(this).text());
+//    console.log("test");
+// });
